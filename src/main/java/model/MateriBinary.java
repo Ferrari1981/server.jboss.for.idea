87f42eb7c2fbaaa2409b2e1f8bb29669525896eb
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2023-07-14 08:49:40 +0300

    sous.jboss.idea----14.07.2023---8.48---провде формирование Blob Image  Успешно-----.zip

--- src/main/java/model/MateriBinary.java
+++ src/main/java/model/MateriBinary.java
@@ -3,8 +3,11 @@ package model;
 
 
 import org.hibernate.annotations.OptimisticLockType;
+import org.hibernate.annotations.Type;
 
 import java.io.Serializable;
+import java.sql.Blob;
+import java.sql.Date;
 import javax.persistence.*;
 
 
@@ -29,12 +32,55 @@ public class MateriBinary implements Serializable {
     @Column(name = "id", nullable = false)
     private Integer id;
 
+
+
+
+    /*@Type(type = "org.hibernate.type.BlobType")
+    @Lob
+    @Column(name = "image", nullable = true)
+    protected java.sql.Blob image;*/
+
+    @Lob
+    @Column(name = "image", columnDefinition="BLOB")
+    private byte[] image;
+
+
+/*
+    @Type(type = "org.hibernate.type.BlobType")
+    @Lob
+    @Column(name = "files", nullable = true)
+    protected java.sql.Blob files;*/
+
+
+    @Lob
+    @Column(name = "files", columnDefinition="BLOB")
+    private byte[] files;
+
+
+
+    @Basic
+    @Column(name = "date_update", nullable = true)
+    private Date dateUpdate;
+
+
     @Basic
     @Column(name = "uuid", nullable = false, precision = 0)
     private long uuid;
 
-    @Lob
-    protected java.sql.Blob image;
+    @Basic
+    @Column(name = "parent_uuid", nullable = true, precision = 0)
+    private Long parentUuid;
+
+    @Basic
+    @Column(name = "user_update", nullable = true)
+    private Integer userUpdate;
+
+    @Basic
+    @Column(name = "current_table", nullable = true, precision = 0)
+    private Long currentTable;
+
+
+
 
 
 
@@ -56,13 +102,52 @@ public class MateriBinary implements Serializable {
 
         this.uuid = uuid;
     }
-    public java.sql.Blob getImage() {
+    public byte[] getImage() {
 
         return image;
     }
 
-    public void setImage(java.sql.Blob image) {
+    public void setImage(byte[] image) {
         this.image = image;
     }
 
+    public byte[] getFiles() {
+        return files;
+    }
+
+    public void setFiles(byte[] files) {
+        this.files = files;
+    }
+
+    public Date getDateUpdate() {
+        return dateUpdate;
+    }
+
+    public void setDateUpdate(Date dateUpdate) {
+        this.dateUpdate = dateUpdate;
+    }
+
+    public Long getParentUuid() {
+        return parentUuid;
+    }
+
+    public void setParentUuid(Long parentUuid) {
+        this.parentUuid = parentUuid;
+    }
+
+    public Integer getUserUpdate() {
+        return userUpdate;
+    }
+
+    public void setUserUpdate(Integer userUpdate) {
+        this.userUpdate = userUpdate;
+    }
+
+    public Long getCurrentTable() {
+        return currentTable;
+    }
+
+    public void setCurrentTable(Long currentTable) {
+        this.currentTable = currentTable;
+    }
 }
