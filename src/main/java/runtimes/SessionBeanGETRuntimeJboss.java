914f61f5505ad16a4e349b94251885f8da7440d3
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2023-10-13 16:32:07 +0300

    sous.jboss.idea---09.10.2023----12.27---переделал вызов транзакции--.zip

---
+++ src/main/java/runtimes/SessionBeanGETRuntimeJboss.java
@@ -0,0 +1,212 @@
+package runtimes;
+
+import businesslogic.BEANCallsBack;
+import businesslogic.CommitSessionHibernate;
+import businesslogic.StreamJSONJacksons;
+import businesslogic.SubClassWriterErros;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.sun.istack.NotNull;
+import dsu1glassfishatomic.workinterfaces.ProducedCard;
+import org.hibernate.*;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
+import org.jboss.ejb3.annotation.TransactionTimeout;
+
+import javax.ejb.*;
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.TransactionScoped;
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+@Stateless(mappedName = "SessionBeanGETRuntimeJboss")
+@LocalBean
+@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+@TransactionTimeout(value = 1, unit = TimeUnit.HOURS)
+public class SessionBeanGETRuntimeJboss {// extends WITH
+
+    private ServletContext ЛОГ;
+    //private	Connection conn; ////// общий коннект для всего севлтера
+    private Statement stmt;
+    @SuppressWarnings("unused")
+    private String ПубличноеHeaderИмя = null;
+    @SuppressWarnings("unused")
+    private String ОшибкаВМетодеdoPOST = new String();
+    private int КоличествоСтрокКоторыеМыОтправимНаКлиент;
+    private String ИмяПолученныйИзSQlServerПосик = null;
+    @SuppressWarnings("unused")
+    private String ПарольПолученныйОтКлиента = null;
+    private String ЛогинПолученныйОтКлиента = null;
+
+    @SuppressWarnings("unused")
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+
+
+    @Inject
+    BEANCallsBack bEANCallsBack;
+    @Inject
+    SubClassWriterErros subClassWriterErros;
+
+    @Inject @ProducedCard
+    SessionFactory sessionSousJboss;
+
+    private Session session;
+    @Inject
+    StreamJSONJacksons streamJSONJacksons ;
+
+    public SessionBeanGETRuntimeJboss() {
+
+        System.out.println("Конструктор  SessionBeanGETRuntimeJboss");
+
+    }
+
+    @Asynchronous
+    public void МетодГлавныйRuntimeJboss(@NotNull ServletContext ЛОГ,
+                                                 @NotNull HttpServletRequest request,
+                                                 @NotNull  HttpServletResponse response) throws InterruptedException, ExecutionException {;
+        try {
+            ///Todo  получаем данные от клиента
+            byte[] БуферРезультатRuntime= 	 МетодЗапускаRuntime(request,ЛОГ,response);
+            ///Todo получаем данные от Клиента на Сервер
+            bEANCallsBack.МетодBackДанныеКлиентуByte(response, БуферРезультатRuntime, ЛОГ ,request );
+            ЛОГ.log("\n"+" Starting.... class "+Thread.currentThread().getStackTrace()[2].getClassName() +"\n"+
+                    " metod "+Thread.currentThread().getStackTrace()[2].getMethodName() +"\n"+
+                    " line "+  Thread.currentThread().getStackTrace()[2].getLineNumber()+"\n"+"  БуферРезультатRuntime  "
+                    + БуферРезультатRuntime );
+        } catch (Exception e) {
+            subClassWriterErros.
+                    МетодаЗаписиОшибкиВЛог(e,
+                            Thread.currentThread().
+                                    getStackTrace(),
+                            ЛОГ,"ErrorsLogs/ErrorJbossServletRuntime.txt");
+        }
+    }
+
+
+
+
+
+
+
+    @SuppressWarnings({ "unused", "deprecation", "rawtypes", "unchecked" })
+    public byte[] МетодЗапускаRuntime(@NotNull HttpServletRequest request,
+                                                    @NotNull ServletContext ЛОГ,
+                                                    @NotNull  HttpServletResponse response) throws SecurityException, SQLException {
+        // TODO Auto-generated method stub
+        byte[] БуферCallsBackДляAndroid = new byte[0];
+        try  {
+            List<?>         ЛистДанныеОтHibenide = null;
+            this.ЛОГ = ЛОГ;
+            // TODO
+            this.request = request;
+            /// TODO
+            this.response = response;
+            // TODO Коннектимся к Базе SQl Server
+            ЛОГ.log("ЛОГ  GET() " + ЛОГ + " request " + request + " response " + response);
+            ////
+            // TODO получаем session
+            ЛОГ.log("ЗАПУСКАЕТСЯ....... ГЛАВНЫЙ МЕТОД GET() СЕРВЛЕТА " + new Date() + "\n" + ЛОГ.getServerInfo()
+                    + "  request " + request + " response " + response + " ЛОГ" + ЛОГ);
+
+
+
+
+            // TODO ГАЛВНЫЙ МЕТОД GET НАЧИНАЕТ РАБОТАТЬ
+            String   JobForServer = Optional.ofNullable(request.getParameter("JobForServer")).orElse("");
+            if (JobForServer.length()>0) {
+                // TODO: 10.03.2023 получение сессиии HIREBIANTE
+                session=   sessionSousJboss.getCurrentSession();
+
+                ЛОГ.log("\n" + " class " + Thread.currentThread().getStackTrace()[2].getClassName() + "\n" +
+                        " metod " + Thread.currentThread().getStackTrace()[2].getMethodName() + "\n" +
+                        " line " + Thread.currentThread().getStackTrace()[2].getLineNumber() + "\n"
+                        + "    session.getTransaction().getStatus() " +  session.getTransaction().getStatus());
+
+                // TODO: 17.03.2023 ЗАПУСКАЕТ ТРАНЗАКЦИЮ BEGIN
+                // TODO: 23.04.2023 ЗапускТарнзакции
+                new CommitSessionHibernate().   методЗапускТранзакции(ЛОГ,session );
+
+
+    }
+
+
+
+
+            switch (JobForServer) {
+                // TODO ЗАДАНИЕ ДЛЯ СЕРВЕР JOBSERVERTASK #5
+                case "Хотим Получить Статус Реальной Работы SQL SERVER":
+                    // TODO РЕАЛЬНЫЙ СТАТУС РАБОТЫ SQL SERVER
+                       ЛистДанныеОтHibenide = Метод_РеальнаяСтатусSqlServer();
+                    ЛОГ.log(" Отправили Хотим Получить Статус Реальной Работы SQL SERVER  JobForServer " + JobForServer
+                            + " ЛистДанныеОтHibenide "
+                            + ЛистДанныеОтHibenide.size());
+                    break;
+                // TODO ЗАДАНИЯ ДЛЯ СЕРВЕРА НЕТУ
+                default:
+                    ЛОГ.log("\n"+"  default:  Starting.... class "+Thread.currentThread().getStackTrace()[2].getClassName() +"\n"+
+                            " metod "+Thread.currentThread().getStackTrace()[2].getMethodName() +"\n"+
+                            " line "+  Thread.currentThread().getStackTrace()[2].getLineNumber()+"\n"+
+                            "Метод_РеальнаяСтатусSqlServer  ЛистДанныеОтHibenide ");
+                    break;
+            }
+            if ( ЛистДанныеОтHibenide!=null && ЛистДанныеОтHibenide.size()>0 ) {
+                БуферCallsBackДляAndroid = streamJSONJacksons.getStreamJacksons(ЛистДанныеОтHibenide);
+            }
+            // TODO
+            ЛОГ.log("БуферCallsBackДляAndroid.toString() " + "" + БуферCallsBackДляAndroid.toString() + " ЛистДанныеОтHibenide " +ЛистДанныеОтHibenide);
+
+          new CommitSessionHibernate().  МетодЗакрываемСессиюHibernate(ЛОГ,session);
+            /////// ошибки метода doGET
+        } catch (Exception e) {
+            subClassWriterErros.
+                    МетодаЗаписиОшибкиВЛог(e,
+                            Thread.currentThread().
+                                    getStackTrace(),
+                            ЛОГ,"ErrorsLogs/ErrorJbossServletRuntime.txt");
+
+        }
+        return  БуферCallsBackДляAndroid;
+        // AsyncResult<StringBuffer>(БуферCallsBackДляAndroid);
+    }
+
+    // TODO реальный статус POST SQl Servera
+    protected List<model.UsersEntity> Метод_РеальнаяСтатусSqlServer() {
+        List<model.UsersEntity> ЛистДанныеОтHibenide  = new ArrayList<>();
+        try {
+            ЛистДанныеОтHibenide =( List<model.UsersEntity>)
+                    session.createQuery("SELECT   usersentity. id FROM model.UsersEntity  as  usersentity WHERE usersentity.rights =:rights   ")
+                            .setParameter("rights",2).setMaxResults(1).getResultList();
+            ЛОГ.log("\n"+" Starting.... class "+Thread.currentThread().getStackTrace()[2].getClassName() +"\n"+
+                    " metod "+Thread.currentThread().getStackTrace()[2].getMethodName() +"\n"+
+                    " line "+  Thread.currentThread().getStackTrace()[2].getLineNumber()+"\n"+
+                    "Метод_РеальнаяСтатусSqlServer  ЛистДанныеОтHibenide " +ЛистДанныеОтHibenide.size());
+        } catch (Exception e) {
+            subClassWriterErros.
+                    МетодаЗаписиОшибкиВЛог(e,
+                            Thread.currentThread().
+                                    getStackTrace(),
+                            ЛОГ,"ErrorsLogs/ErrorJbossServletDSU1.txt");
+        }
+        return ЛистДанныеОтHibenide;
+    }
+
+
+
+
+    // TODO: 23.04.2023 end test code
+}
